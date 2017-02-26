6207173e97e40603e34e13e3b8ae06d18199f4ce
Charles Ko <samannaphala@gmail.com>
2017-02-26 16:45:04 -0500

    Forms

--- populate_rango.py
+++ populate_rango.py
@@ -10,18 +10,18 @@ def populate():
 # we want to add into each category.
 
     python_pages = [
-        {"title":"Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/"},
-        {"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/"},
-        {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/"} ]
+        {"title":"Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views":48},
+        {"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views":16},
+        {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/", "views":0} ]
 
     django_pages = [
-        {"title":"Official Django Tutorial", "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-        {"title":"Django Rocks", "url":"http://www.djangorocks.com/"},
-        {"title":"How to Tango with Django", "url":"http://www.tangowithdjango.com/"} ]
+        {"title":"Official Django Tutorial", "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views":24},
+        {"title":"Django Rocks", "url":"http://www.djangorocks.com/", "views":0},
+        {"title":"How to Tango with Django", "url":"http://www.tangowithdjango.com/", "views":0} ]
 
     other_pages = [
-        {"title":"Bottle", "url":"httpL//bottlepy.org/docs/dev/"},
-        {"title":"Flask", "url":"http://flask.pocoo.org"} ]
+        {"title":"Bottle", "url":"httpL//bottlepy.org/docs/dev/", "views":30},
+        {"title":"Flask", "url":"http://flask.pocoo.org", "views":2} ]
 
     # Nest those lists of dictionaries into another dictionary
     cats = {
@@ -32,13 +32,13 @@ def populate():
     for cat, cat_data in cats.iteritems(): # for key, value in cats dictionary
         c = add_cat(cat, cat_data["views"], cat_data["likes"]) # c is returned from add_cat function
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,55 @@
 from django.shortcuts import render
+from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
 
-    return render(request, 'rango/index.html', context=context)
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    return render(request, 'rango/index.html', context_dict)
+
+def show_category(request, category_name_slug):
+	context_dict = {}
+
+	try:
+		# Can we find a category name slug with the given name?
+		# If we can't, the .get() method raises a DoesNotExist exception
+		category = Category.objects.get(slug=category_name_slug)
+
+		# Retrieve all of the associated pages.
+		# Note that filter() will return a list of page objects or an empty list
+		pages = Page.objects.filter(category=category)
+
+		# Adds our results list to the template context under name pages
+		context_dict['pages'] = pages
+
+		# We also add the category object from the database to the context dict
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	form = CategoryForm()
+
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print(form.errors)
+
+	return render(request, 'rango/add_category.html', {'form': form})
+
 
 def about(request):
     context = {'boldmessage': "My name is Freida. That's all you have to know."}
